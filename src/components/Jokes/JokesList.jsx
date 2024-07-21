66c711032819c19f8a5173b31d42d418ce35a4a4
Muhammad Abdullah <anasbutt2014@gmail.com>
2024-07-21 16:09:13 +0400

    6 commit

--- src/components/Jokes/JokesList.jsx
+++ src/components/Jokes/JokesList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./JokeLists.css";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, CircularProgress } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import Joke from "./Joke";
 import { fetchJokes, voteJoke } from "../../actions/jokes";
@@ -24,9 +24,27 @@ export default function App() {
 
   if (loading) {
     return (
-      <>
-        <h1>Loading...</h1>
-      </>
+      <Box className="jokesList">
+        <Box className="jokesListSidebar">
+          <Box className="jokeListTitle">
+            <div>
+              <h1>Dad</h1>
+              <h1>Jokes</h1>
+            </div>
+          </Box>
+          <h1 className="w-96 shadow-lg">😆</h1>
+          <Button
+            variant="contained"
+            onClick={handleFetchJokes}
+            className="button-fetch"
+          >
+            Fetch Jokes
+          </Button>
+        </Box>
+        <Box className="jokesListJokes">
+          <CircularProgress />
+        </Box>
+      </Box>
     );
   }
 
